6c6fe4cfd620738348711d71a04fcac93ec090bf
Milica Simović <85285657+MiliSii@users.noreply.github.com>
2022-07-14 10:23:55 +0200

    update

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @EnableAutoConfiguration
 public class DemoApplication {
-
+ 
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
