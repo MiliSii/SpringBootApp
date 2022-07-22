4d2f7ba5e875257504114b98768126df4194293f
Milica Simović <85285657+MiliSii@users.noreply.github.com>
2022-07-22 10:07:35 +0200

    update

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.advice.controller.APIControlers;
-import com.example.demo.model.HelloEntity;
-import com.example.demo.service.HelloService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hibernate.mapping.List;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @SpringBootApplication
 @EnableWebSecurity
--- demo/src/test/java/com/example/demo/JUnitSetUp.java
+++ demo/src/test/java/com/example/demo/JUnitSetUp.java
@@ -5,12 +5,11 @@ import static org.junit.Assert.assertEquals;
 
 public class JUnitSetUp {
 
-
         @Test
-
         public void testAdd() {
             String str = "Junit is working fine";
             assertEquals("Junit is working fine",str);
         }
+
     }
 
